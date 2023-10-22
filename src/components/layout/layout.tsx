eb1167b6d78aaa06343eeadd1c657b46cff62629
Sajjad <titanxl79@gmail.com>
2023-10-22 09:43:30 +0330

    add layout  and working on customization

---
+++ src/components/layout/layout.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import { Layout, Menu, theme } from 'antd';
+
+const { Header, Content, Footer, Sider } = Layout;
+
+const LayoutForm: React.FC = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  return (
+    <Layout	style={{minHeight: '100vh'}}>
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+      >
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['4']}
+          items={[UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
+            (icon, index) => ({
+              key: String(index + 1),
+              icon: React.createElement(icon),
+              label: `nav ${index + 1}`,
+            }),
+          )}
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0 }} className='bg-sky-200'/>
+        <Content style={{ margin: '24px 16px 0' }}>
+          <div style={{ padding: 24, minHeight: 360, background: colorBgContainer }}>content
+          </div>
+        </Content>
+        <Footer style={{textAlign: 'center'}}>تمامی حقوق برای شرکت digitkey می باشد.</Footer>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default LayoutForm;
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,35 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from "react-router-dom";
+import {ConfigProvider} from "antd";
+import fa_IR from "antd/lib/locale/fa_IR";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ConfigProvider locale={fa_IR} direction="rtl" theme={{
+        token:{
+            colorBgLayout:'rgb(186 230 253)'
+
+        },
+        components: {
+            Layout: {
+                triggerColor:'rgb(14 165 233)',
+            },
+            Divider: {
+                colorSplit: '#69b1ff'
+            }
+        }
+    }}>
+        <BrowserRouter>
+          <React.StrictMode>
+            <App />
+          </React.StrictMode>
+        </BrowserRouter>
+    </ConfigProvider>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
