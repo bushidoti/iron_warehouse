52522fc973dad260f1e27834b80f0fd9cb2f307e
Sajjad <titanxl79@gmail.com>
2023-10-29 17:53:36 +0330

    assign urls to menu items

--- src/components/layout/menu_items.tsx
+++ src/components/layout/menu_items.tsx
@@ -7,7 +7,6 @@ import {
 import SellIcon from '@mui/icons-material/Sell';
 import WarehouseIcon from '@mui/icons-material/Warehouse';
 import ConstructionIcon from '@mui/icons-material/Construction';
-import BackupIcon from '@mui/icons-material/Backup';
 import HomeSharpIcon from '@mui/icons-material/HomeSharp';
 
 type MenuItem = Required<MenuProps>['items'][number];
@@ -34,50 +33,43 @@ function getItem(
     } as MenuItem;
 }
 
-
 export const items: MenuItem[] = [
     getItem(<Link to='/'>خانه</Link>, '1', <HomeSharpIcon/>),
-    getItem(<Link target={"_blank"} to='https://api.oghab-asaluyeh.ir/admin/'>پنل مدیریت</Link>, '2', <PieChartOutlined/> ,  undefined ),
-
-    getItem(<Link target={"_blank"} to='http://www.oghab-asaluyeh.ir:2082/cpsess6008508683/frontend/
-  jupiter/backup/wizard-backup-type.html?login=1&post_login=3837540636687'>بکاپ</Link>
-        , '3', <BackupIcon/> ,  undefined ),
-
+    getItem(<Link target={"_blank"} to='http://127.0.0.1:8000/admin/'>پنل مدیریت</Link>, '2', <PieChartOutlined/> ,  undefined ),
     getItem('انبارداری', 'sub1', <WarehouseIcon/>, [
-
-        getItem(<Link to='../contract'>انبار صنعتی</Link>, 'sub2', undefined , [
-            getItem(<Link to='../contract/register'>ثبت</Link>, '4'),
-            getItem(<Link to='../contract/report'>گزارش</Link>, '5'),
-            getItem(<Link to='../contract/upload'>باگذاری</Link>, '6'),
+        getItem(<Link to='../industrial_inventory'>انبار صنعتی</Link>, 'sub2', undefined , [
+            getItem(<Link to='../industrial_inventory/register'>ثبت</Link>, '4'),
+            getItem(<Link to='../industrial_inventory/report'>گزارش</Link>, '5'),
+            getItem(<Link to='../industrial_inventory/upload'>باگذاری</Link>, '6'),
         ]),
 
-        getItem(<Link to='../personal'>انبار مصرفی</Link>, 'sub3', undefined, [
-            getItem(<Link to='../personal/register'>ثبت</Link>, '7'),
-            getItem(<Link to='../personal/report'>گزارش</Link>, '8'),
-            getItem(<Link to='../personal/upload'>باگذاری</Link>, '9'),
+        getItem(<Link to='../consumable_inventory'>انبار مصرفی</Link>, 'sub3', undefined, [
+            getItem(<Link to='../consumable_inventory/register'>ثبت</Link>, '7'),
+            getItem(<Link to='../consumable_inventory/report'>گزارش</Link>, '8'),
+            getItem(<Link to='../consumable_inventory/upload'>باگذاری</Link>, '9'),
         ]),
-        getItem(<Link to='../personal'>انبار محصول</Link>, '10', undefined),
-        getItem(<Link to='../personal'>انبار ضایعات</Link>, '11', undefined),
-
-        getItem(<Link to='../personal'>اموال</Link>, 'sub4', undefined, [
-            getItem(<Link to='../personal/register'>ثبت</Link>, '12'),
-            getItem(<Link to='../personal/report'>گزارش</Link>, '13'),
-            getItem(<Link to='../personal/upload'>باگذاری</Link>, '14'),
+        getItem(<Link to='../product_inventory'>انبار محصول</Link>, '10', undefined),
+        getItem(<Link to='../waste_inventory'>انبار ضایعات</Link>, '11', undefined),
+        getItem(<Link to='../property'>اموال</Link>, 'sub4', undefined, [
+            getItem(<Link to='../property/register'>ثبت</Link>, '12'),
+            getItem(<Link to='../property/report'>گزارش</Link>, '13'),
+            getItem(<Link to='../property/upload'>باگذاری</Link>, '14'),
         ]),
     ]),
-    getItem(<Link to='../warhouse'>تولید</Link>, 'sub5', <ConstructionIcon/>, [
-        getItem(<Link to='../warhouse/product'>مدیریت تولید</Link>, 'sub6', null, [
-            getItem(<Link to='../warhouse/product/register'>ثبت</Link>, '15'),
-            getItem(<Link to='../warhouse/product/report'>گزارش</Link>, '16'),
+    getItem("تولید", 'sub5', <ConstructionIcon/>, [
+        getItem(<Link to='../production/product'>مدیریت تولید</Link>, 'sub6', null, [
+            getItem(<Link to='../production/register'>ثبت</Link>, '15'),
+            getItem(<Link to='../production/report'>گزارش</Link>, '16'),
         ]),
 
-        getItem(<Link to='../warhouse/property'>انبار تولید</Link>, 'sub7', null, [
-            getItem(<Link to='../warhouse/property/register'>ثبت</Link>, '17'),
-            getItem(<Link to='../warhouse/property/report'>گزارش</Link>, '18'),
-            getItem(<Link to='../warhouse/property/sent'>ارسالی</Link>, '19'),
-            getItem(<Link to='../warhouse/property/recycle'>بایگانی</Link>, '20'),
+        getItem(<Link to='../production_inventory'>انبار تولید</Link>, 'sub7', null, [
+            getItem(<Link to='../production_inventory/register'>ثبت</Link>, '17'),
+            getItem(<Link to='../production_inventory/report'>گزارش</Link>, '18'),
+            getItem(<Link to='../production_inventory/sent'>ارسالی</Link>, '19'),
+            getItem(<Link to='../production_inventory/recycle'>بایگانی</Link>, '20'),
         ]),
     ] ),
-    getItem(<Link to='../logout'>فروش</Link>, '21', <SellIcon/>),
-    getItem(<Link to='../logout'>خروج</Link>, '22', <PoweroffOutlined/>,undefined,undefined,true),
+    getItem(<Link to='../sale'>فروش</Link>, '21', <SellIcon/>),
+    getItem(<Link to='../buy'>درخواست خرید</Link>, '22', <SellIcon/>),
+    getItem(<Link to='../logout'>خروج</Link>, '23', <PoweroffOutlined/>,undefined,undefined,true),
 ];
