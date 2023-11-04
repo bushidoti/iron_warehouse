4c62d52b826fcf4d50da5eef824c970c58fe0122
Sajjad <titanxl79@gmail.com>
2023-11-04 11:06:08 +0330

    customize style of table for print

--- src/components/warehouse/industrial_warehouse/register/table.tsx
+++ src/components/warehouse/industrial_warehouse/register/table.tsx
@@ -13,66 +13,72 @@ const TablePrint = (props: {
                 ref={props.componentPDF} style={{direction: 'rtl', fontSize: '.56vw'}}>
                 <thead>
                     <tr>
-                        <td colSpan={1} className='p-1 border-solid border text-center'>
-                                گروه تولیدی صنعتی
+                        <td colSpan={3} className="p-3 border-solid border">
+                              <div className="w-full h-[40px] ">
+                                <img
+                                    alt=''
+                                    className='object-cover float-right	 h-[50px] w-full'
+                                    src={require('../../../../assets/printLogo.png')}
+                                />
+                           </div>
                         </td>
-                        <td colSpan={5} className='p-1 border-solid border text-center'>
-                           <Text className='text-[10px]' strong>
+                        <td colSpan={3} className="ps-1 border-solid border text-center">
+                            <Text className='text-[12px]' strong>
                                 حواله ورود کالا به انبار
                            </Text>
                         </td>
-                        <td colSpan={1} className='p-1 border-solid border'>
-                            <Space size={2} direction="vertical">
-                                 <Text className='text-[8px]'> شماره: {props.productSub[0]?.systemID} </Text>
-                                 <Text className='text-[8px]'>تاریخ: {dayjs().locale('fa').format('DD-MM-YYYY')}</Text>
-                            </Space>
+                        <td colSpan={1} className="ps-4 border-solid border  text-right">
+                               <Space size={30} direction="vertical">
+                                     <Text className='text-[8px]'> شماره: {props.productSub[0]?.systemID} </Text>
+                                     <Text className='text-[8px]'>تاریخ: {dayjs().locale('fa').format('DD-MM-YYYY')}</Text>
+                               </Space>
                         </td>
                     </tr>
                     <tr>
-                        <td colSpan={5}>
+                        <td colSpan={5} className='text-[10px] font-extrabold'>
                                 نام طرف حساب :
                         </td>
                     </tr>
                     <tr>
-                        <td colSpan={5} className='border-solid border text-center'>
-                              <Text className='text-[7px]' strong>
+                        <td colSpan={5} className='border-solid border text-center font-extrabold'>
+                              <Text className='text-[10px]' strong>
                                   توسط انباردار تکمیل گردد
                               </Text>
                         </td>
-                        <td colSpan={2} className='border-solid border text-center'>
-                             <Text className='text-[7px]' strong>
+                        <td colSpan={2} className='border-solid border text-center font-extrabold'>
+                             <Text className='text-[10px]' strong>
                                   توسط حسابداری تکمیل گردد
                             </Text>
                         </td>
                     </tr>
                     <tr>
-                        <th className='border-solid	border' scope="col">ردیف</th>
-                        <th className='border-solid	border' scope="col">کد کالا</th>
-                        <th className='border-solid	border' scope="col">نام کالا</th>
-                        <th className='border-solid	border' scope="col">تعداد کارتن</th>
-                        <th className='border-solid	border' scope="col">تعداد واحد</th>
-                        <th className='border-solid	border' scope="col">نرخ</th>
-                        <th className='border-solid	border' scope="col">مبلغ</th>
+                        <th className='text-[12px] border-solid	border font-extrabold' scope="col">ردیف</th>
+                        <th className='text-[12px] border-solid	border font-extrabold' scope="col">کد کالا</th>
+                        <th className='text-[12px] border-solid	border font-extrabold' scope="col">نام کالا</th>
+                        <th className='text-[12px] border-solid	border font-extrabold' scope="col">تعداد کارتن</th>
+                        <th className='text-[12px] border-solid	border font-extrabold' scope="col">تعداد واحد</th>
+                        <th className='text-[12px] border-solid	border font-extrabold' scope="col">نرخ</th>
+                        <th className='text-[12px] border-solid	border font-extrabold' scope="col">مبلغ</th>
                     </tr>
                 </thead>
 
                 <tbody>
                 {props.productSub.map((data, i) => (
                     <tr key={data.product}>
-                        <th className='border-solid	border' scope="row">{i+1}</th>
-                        <td className='border-solid	border text-center'>{data.product}</td>
-                        <td className='border-solid	border text-center'>{data.name}</td>
-                        <td className='border-solid	border text-center'>{data.carton}</td>
-                        <td className='border-solid	border text-center'>{data.input}</td>
-                        <td className='border-solid	border text-center'>{`${data.rate}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}</td>
-                        <td className='border-solid	border text-center'>{`${data.rate * data.input}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}</td>
+                        <th className='text-[12px] border-solid	border font-extrabold' scope="row">{i+1}</th>
+                        <td className='text-[12px] border-solid	border text-center font-extrabold'>{data.product}</td>
+                        <td className='text-[12px] border-solid	border text-center'>{data.name}</td>
+                        <td className='text-[12px] border-solid	border text-center'>{data.carton}</td>
+                        <td className='text-[12px] border-solid	border text-center'>{data.input}</td>
+                        <td className='text-[12px] border-solid	border text-center'>{`${data.rate}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}</td>
+                        <td className='text-[12px] border-solid	border text-center'>{`${data.rate * data.input}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}</td>
                     </tr>
                 ))
                 }
                     <tr>
                         <td colSpan={5}>
                             <Text className='text-[10px]' strong>
-                              کالای فوق مطابق لیست تجویل گرفته شد.
+                              کالای فوق مطابق لیست تحویل گرفته شد.
                             </Text>
                         </td>
                     </tr>
@@ -80,19 +86,19 @@ const TablePrint = (props: {
                       <td colSpan={7}>
                           <Flex>
                              <div className="w-1/3 ps-1 border-solid border grid grid-cols-2">
-                                  <div className="col-start-1 col-span-4"><Text className='text-[8px]'>نام تحویل گیرنده(انبار):</Text></div>
+                                  <div className="col-start-1 col-span-4"><Text className='text-[10px]'>نام تحویل گیرنده(انبار):</Text></div>
                                   <div className="col-start-1 col-span-4"></div>
-                                  <div className="col-end-4 me-2"><Text className='text-[8px]'>امضاء</Text></div>
+                                  <div className="col-end-3 ms-10"><Text className='text-[10px]'>امضاء</Text></div>
                              </div>
                              <div className="w-1/3 ps-1 border-solid border grid grid-cols-2">
-                                  <div className="col-start-1 col-span-4"><Text className='text-[8px]'>نام تحویل دهنده:</Text></div>
+                                  <div className="col-start-1 col-span-4"><Text className='text-[10px]'>نام تحویل دهنده:</Text></div>
                                   <div className="col-start-1 col-span-4"></div>
-                                  <div className="col-end-4 me-2"><Text className='text-[8px]'>امضاء</Text></div>
+                                  <div className="col-end-3 ms-10"><Text className='text-[10px]'>امضاء</Text></div>
                              </div>
                              <div className="w-1/3 ps-1 border-solid border grid grid-cols-2">
-                                  <div className="col-start-1 col-span-4"><Text className='text-[8px]'>نام تأیید کننده(مدیریت):</Text></div>
+                                  <div className="col-start-1 col-span-4"><Text className='text-[10px]'>نام تأیید کننده(مدیریت):</Text></div>
                                   <div className="col-start-1 col-span-4"></div>
-                                  <div className="col-end-4 me-2"><Text className='text-[8px]'>امضاء</Text></div>
+                                  <div className="col-end-3 ms-10"><Text className='text-[10px]'>امضاء</Text></div>
                              </div>
                           </Flex>
                       </td>
